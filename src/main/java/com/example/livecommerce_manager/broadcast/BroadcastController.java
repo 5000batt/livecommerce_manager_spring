281c18766842aefa4a93324a736d64afe6478d53
5000batt <5000batt@gmail.com>
2021-04-07 17:10:16 +0900

    eureka gateway

--- src/main/java/com/example/livecommerce_manager/broadcast/BroadcastController.java
+++ src/main/java/com/example/livecommerce_manager/broadcast/BroadcastController.java
@@ -15,6 +15,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
